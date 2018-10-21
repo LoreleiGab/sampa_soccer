bd447e089d857a81b4a29d304a313e66a1007120
Lorelei Gabriele <loreleigab@hotmail.com>
2018-10-21 00:57:26 -0300

    Cadastro de base

---
+++ perfil/adm/base_edit.php
@@ -0,0 +1,143 @@
+<?php
+include "includes/menu.php";
+$con = bancoMysqli();
+
+if(isset($_POST['cadastrar']) || isset($_POST['editar'])){
+    $nome = $_POST['nome'];
+    $apelido = $_POST['apelido'];
+    $posicao = $_POST['posicao'];
+    $pe_dominante = $_POST['pe_dominante'];
+    $data_nascimento = $_POST['data_nascimento'];
+    $telefone01 = $_POST['telefone01'];
+    $telefone02 = $_POST['telefone02'];
+    $email = $_POST['email'];
+    $restricao = $_POST['restricao'];
+    $diagnostico = $_POST['diagnostico'];
+    $classificacao_id = 2;
+    $usuario_id = $_SESSION['idUser'];
+}
+
+if(isset($_POST['cadastrar'])){
+    $sql_cliente = "INSERT INTO clientes (nome, data_nascimento,  telefone01, telefone02, emal, diagnostico, classificacao_id, usuario_id) VALUES ('$nome', '$data_nascimento', '$telefone01', '$telefone02', '$email', '$diagnostico', '$classificacao_id', '$usuario_id')";
+    if(mysqli_query($con,$sql_cliente)){
+        $idCliente = recuperaUltimo("clientes");
+        $sql_base = "INSERT INTO base (apelido, posicao, pe_dominante, restricao, cliente_id) VALUES ('$apelido', '$posicao', '$pe_dominante', '$restricao', '$idCliente')";
+        if(mysqli_query($con,$sql_base)) {
+            $mensagem = mensagem("success", "Cadastrado com sucesso!");
+        }
+        else{
+            $mensagem = mensagem("danger","Erro ao gravar! Tente novamente.");
+        }
+    }
+    else{
+        $mensagem = mensagem("danger","[COD2]Erro ao gravar! Tente novamente.").$sql_cliente;
+    }
+}
+if(isset($_POST['editar'])){
+    $idCliente = $_POST['idCliente'];
+    $sql_edita_cliente = "UPDATE clientes SET nome = '$nome', data_nascimento = '$data_nascimento', telefone01 = '$telefone01', telefone02 = '$telefone02', emal = '$email', diagnostico = '$diagnostico', classificacao_id = '$classificacao_id' WHERE id = '$idCliente'";
+    if(mysqli_query($con,$sql_edita_cliente)){
+        $sql_edita_base = "UPDATE base SET apelido = '$apelido', posicao = '$posicao', pe_dominante = '$pe_dominante', restricao = '$restricao' WHERE cliente_id = '$idCliente'";
+        if(mysqli_query($con,$sql_edita_base)) {
+            $mensagem = mensagem("success", "Gravado com sucesso!");
+        }
+        else{
+            $mensagem = mensagem("danger","Erro ao gravar! Tente novamente.");
+        }
+    }
+    else{
+        $mensagem = mensagem("danger","[COD2]Erro ao gravar! Tente novamente.");
+    }
+}
+
+if(isset($_POST['carregar'])){
+    $idCliente = $_POST['idCliente'];
+}
+
+$cliente = recuperaDados("clientes","id",$idCliente);
+$base = recuperaDados("base","cliente_id",$idCliente);
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- START FORM-->
+        <h2 class="page-header">Cliente</h2>
+
+        <div class="row">
+            <div class="col-md-12">
+                <!-- general form elements -->
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Cadastro de Base</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <div class="row" align="center">
+                        <?php if(isset($mensagem)){echo $mensagem;};?>
+                    </div>
+                    <form method="POST" action="?perfil=administrador&p=base_edit" role="form">
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="nome">Nome completo</label>
+                                <input type="text" id="nome" name="nome" class="form-control" maxlength="180" value="<?= $cliente['nome'] ?>">
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-2">
+                                    <labeL for="data_nascimento">Data de Nascimento</labeL>
+                                    <input type="date" id="data_nascimento" name="data_nascimento" class="form-control" value="<?= $cliente['data_nascimento'] ?>">
+                                </div>
+                                <div class="form-group col-md-3">
+                                    <labeL for="telefone01">Telefone #1</labeL>
+                                    <input type="text" id="telefone01" name="telefone01" class="form-control" value="<?= $cliente['telefone01'] ?>">
+                                </div>
+                                <div class="form-group col-md-3">
+                                    <labeL for="telefone02">Telefone #2</labeL>
+                                    <input type="text" id="telefone02" name="telefone02" class="form-control" value="<?= $cliente['telefone02'] ?>">
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <labeL for="email">Email</labeL>
+                                    <input type="email" id="email" name="email" class="form-control" value="<?= $cliente['emal'] ?>">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-2">
+                                    <labeL for="apelido">Apelido</labeL>
+                                    <input type="text" id="apelido" name="apelido" class="form-control" value="<?= $base['apelido'] ?>">
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <labeL for="posicao">Posição</labeL>
+                                    <input type="text" id="posicao" name="posicao" class="form-control" value="<?= $base['posicao'] ?>">
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <labeL for="pe_dominante">Pé dominante</labeL>
+                                    <input type="text" id="pe_dominante" name="pe_dominante" class="form-control" value="<?= $base['pe_dominante'] ?>">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <labeL for="restricao">Alguma restrição</labeL>
+                                    <input type="text" id="restricao" name="restricao" class="form-control" maxlength="255" value="<?= $base['restricao'] ?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="diagnostico">Diagnóstico</label>
+                                <textarea class="form-control" rows="5" id="diagnostico" name="diagnostico"><?= $cliente['diagnostico'] ?></textarea>
+                            </div>
+
+                        </div>
+                        <!-- /.box-body -->
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-default">Cancela</button>
+                            <button type="submit" name="cadastrar" class="btn btn-info pull-right">Cadastrar</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
